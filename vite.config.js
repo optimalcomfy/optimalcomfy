102f35662f1fa1465d7553c9dedf79ffee489cab
Amos Billy <amosbillykipchumba@gmail.com>
2025-04-28 19:04:25 +0300

    font awesome

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
-        svgr(), 
+        svgr(),
     ],
     css: {
         devSourcemap: false,
@@ -21,6 +21,11 @@ export default defineConfig({
             output: {
                 assetFileNames: 'assets/[name][extname]',
             },
+            external: [
+                '@fortawesome/react-fontawesome', 
+                '@fortawesome/free-solid-svg-icons', // Add any other relevant FontAwesome packages
+            ]
         },
     },
 });
+

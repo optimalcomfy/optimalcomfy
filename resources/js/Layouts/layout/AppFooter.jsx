7c8a6d153434417d85a051062be5037054221bd7
Amos Billy <amosbillykipchumba@gmail.com>
2025-05-09 10:53:24 +0300

    changes

--- resources/js/Layouts/layout/AppFooter.jsx
+++ resources/js/Layouts/layout/AppFooter.jsx
@@ -4,7 +4,7 @@ const AppFooter = () => {
     return (
         <div className="layout-footer">
             <span>Powered by</span>
-            <a href='https://rRbsitedesigners.co.ke' target='_blank' className="font-medium ml-2">aB</a>
+            <a href='https://optimalcomfy.co.ke' target='_blank' className="font-medium ml-2">Kenya Website designers</a>
         </div>
     );
 };

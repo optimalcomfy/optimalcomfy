588f0f926a3eba729a4402a4d95a62d72f00cdba
Amos Billy <amosbillykipchumba@gmail.com>
2025-05-23 01:10:29 +0300

    pesapal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <title>@yield('title', 'Optimal Comfy')</title>
-    @vite('resources/css/app.css') 
 </head>
 <body class="antialiased">
     @yield('content')

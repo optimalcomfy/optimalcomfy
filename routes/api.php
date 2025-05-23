b4a78c64609735dcd4c7d23285f67b746df6a27a
Amos Billy <amosbillykipchumba@gmail.com>
2025-05-23 11:27:31 +0300

    booking

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-
-Route::get('/pesapal/confirm/{user_id}/{booking_id}/{cycle}', [PesapalController::class, 'verifyPayment']);
-
+Route::get('/pesapal/confirm/{user_id}/{booking_type}/{booking_id}/{cycle}', [PesapalController::class, 'verifyPayment']);

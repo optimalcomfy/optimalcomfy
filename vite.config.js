8acc3a5c09a6f505981986245ce376ac7654b7c0
Amos Billy <amosbillykipchumba@gmail.com>
2025-05-29 23:03:14 +0300

    wallet

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: [
+                'resources/js/app.jsx',
+                'resources/css/app.css', 
+                'node_modules/primeicons/primeicons.css', 
+                'node_modules/primereact/resources/themes/lara-light-indigo/theme.css',
+                'node_modules/primereact/resources/primereact.min.css',
+                'node_modules/primeflex/primeflex.css'
+            ],
             refresh: true,
         }),
         react(),
-        svgr(), 
     ],
-    css: {
-        devSourcemap: false,
-    },
-    build: {
-        assetsDir: 'assets',
-        rollupOptions: {
-            output: {
-                assetFileNames: 'assets/[name][extname]',
-            },
-        },
-    },
 });

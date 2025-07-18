bdb3491071ae0c016a6bc8b97ff541cfb90848ad
Amos Billy <amosbillykipchumba@gmail.com>
2025-07-18 13:16:00 +0300

    stk

--- config/services.php
+++ config/services.php
@@ -40,9 +40,10 @@ return [
     'mpesa' => [
         'consumer_key' => env('MPESA_CONSUMER_KEY'),
         'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
-        'passkey' => env('MPESA_PASSKEY'),
-        'business_shortcode' => env('MPESA_SHORTCODE'),
+        'passkey' => env('MPESA_PASSKEY'), // Optional, if you use passkey for STK
+        'business_shortcode' => env('MPESA_BUSINESS_SHORTCODE'),
         'callback_url' => env('MPESA_CALLBACK_URL'),
-    ]
+        'base_url' => env('MPESA_BASE_URL', 'https://sandbox.safaricom.co.ke'),
+    ],
 
 ];

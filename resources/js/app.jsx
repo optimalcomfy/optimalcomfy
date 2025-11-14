9bab869eaf2b6095c50b110698f999cb4bdf9eed
Amos Billy <amosbillykipchumba@gmail.com>
2025-11-14 04:22:46 +0300

    loading icon

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
 
-
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
@@ -9,30 +8,46 @@ import 'primeflex/primeflex.css';
 
 import '../css/layout.css'
 
-
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import {LayoutProvider} from "@/Layouts/layout/context/layoutcontext.jsx";
-import {PrimeReactProvider} from "primereact/api";
+import { LayoutProvider } from "@/Layouts/layout/context/layoutcontext.jsx";
+import { PrimeReactProvider } from "primereact/api";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Ristay';
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+// Enhanced NProgress Configuration
+const ProgressBar = {
+  delay: 250,
+  color: '#3B82F6',
+  includeCSS: false,
+  showSpinner: true,
 
-        root.render(
-            <PrimeReactProvider>
-            <LayoutProvider>
-            <App {...props} />
-            </LayoutProvider>
-            </PrimeReactProvider>
-        );
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  template: `
+    <div class="bar" role="bar">
+      <div class="peg"></div>
+    </div>
+    <div class="spinner" role="spinner">
+      <div class="spinner-content">
+        <div class="spinner-icon"></div>
+        <div class="spinner-text">Ristay</div>
+      </div>
+    </div>
+  `
+};
+
+createInertiaApp({
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
+    root.render(
+      <PrimeReactProvider>
+        <LayoutProvider>
+          <App {...props} />
+        </LayoutProvider>
+      </PrimeReactProvider>
+    );
+  },
+  progress: ProgressBar
 });

f39297657d360c8a2b56737325d865d3e7f6dcaa
Amos Billy <amosbillykipchumba@gmail.com>
2025-11-14 08:03:16 +0300

    variables working

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,26 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // M-Pesa Callbacks
+        'api/mpesa/stk/callback',
+        'api/mpesa/ride/stk/callback',
+        'api/mpesa/result',
+        'api/mpesa/timeout',
+        'api/refund/callback',
+        'api/refund/timeout',
+        'api/car-refund/callback',
+        'api/car-refund/timeout',
+
+        // Pesapal Callbacks
+        'api/pesapal/callback',
+        'api/pesapal/notification',
+
+        // Other callbacks
         'bookings/store',
-        '/mpesa/result', 
-        '/mpesa/timeout',
+
+        // Add test routes
+        'api/mpesa/test-callback',
+        'api/mpesa/test-get',
+        'api/mpesa/test-any',
     ];
 }
